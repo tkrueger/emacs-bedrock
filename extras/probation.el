97ed52c2c136c24c684a99fc0d92b592ff99f46c
Thorsten Krüger <thorsten.krueger@uni-muenster.de>
2025-02-03 13:25:12 +0100

    probabtion entry for combobulate

--- extras/probation.el
+++ extras/probation.el
@@ -41,4 +41,14 @@ If point was already at that position, move point to beginning of line."
 		   (abbreviate-file-name (buffer-file-name))
 		 "%b"))))
 
+(use-package combobulate
+  :custom
+  ;; You can customize Combobulate's key prefix here.
+  ;; Note that you may have to restart Emacs for this to take effect!
+  (combobulate-key-prefix "C-c o")
+  :hook ((prog-mode . combobulate-mode))
+  ;; Amend this to the directory where you keep Combobulate's source
+  ;; code.
+  :load-path ("/home/thor/work/oss/combobulate"))
+
 ;;; probation.el ends here
