0b63f67a10e149acb01ddf7fd105f5a26ab865d2
Thorsten <thorstenkg@gmail.com>
2025-04-22 20:28:44 +0200

    set re-builder syntax to string
    see https://www.masteringemacs.org/article/re-builder-interactive-regexp-builder

--- init.el
+++ init.el
@@ -265,7 +265,8 @@ If the new path's directories does not exist, create them."
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" default))
- '(package-selected-packages '(aggressive-indent which-key)))
+ '(package-selected-packages '(aggressive-indent which-key))
+ '(reb-re-syntax 'string))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
