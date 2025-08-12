5f8054595a64d3e26bda1166d0076beb3bdf0e84
Thorsten Krüger <thorsten.krueger@uni-muenster.de>
2025-08-12 08:32:45 +0200

    only bind if function is available

--- early-init.el
+++ early-init.el
@@ -27,7 +27,8 @@
 
 ;; Default frame configuration: full screen, good-looking title bar on macOS
 (setq frame-resize-pixelwise t)
-(tool-bar-mode -1)                      ; All these tools are in the menu-bar anyway
+(if (fboundp 'tool-bar-mode)
+    (tool-bar-mode -1))                      ; All these tools are in the menu-bar anyway
 (setq default-frame-alist '((fullscreen . maximized)
 
                             ;; You can turn off scroll bars by uncommenting these lines:
