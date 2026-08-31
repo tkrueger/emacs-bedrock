15e2277688f749f3451908d3d62070c260eace7d
Thorsten <thorstenkg@gmail.com>
2026-08-31 22:05:38 +0200

    treesitter is included in emacs 31.1

--- extras/dev.el
+++ extras/dev.el
@@ -128,7 +128,7 @@
 	(json "https://github.com/tree-sitter/tree-sitter-json")
 	(make "https://github.com/alemuller/tree-sitter-make")
 	(markdown "https://github.com/ikatyang/tree-sitter-markdown")
-	(python "https://github.com/tree-sitter/tree-sitter-python" "v0.23.6")  ;; older tag, we nee ABI 14 for this emacs
+	(python "https://github.com/tree-sitter/tree-sitter-python")
 	(toml "https://github.com/tree-sitter/tree-sitter-toml")
 	(tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
 	(typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
@@ -356,12 +356,13 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (use-package markdown-ts-mode
-  :ensure t
+  ;; built into Emacs >=31; no :ensure -- installing the MELPA package of the
+  ;; same name shadows the built-in and actively refuses to load
   :mode ("\\.md\\'" . markdown-ts-mode)
   :defer 't
   :config
-  (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "v0.4.1" "tree-sitter-markdown/src"))  ;; pinned to ABI 14, matches this emacs
-  (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "v0.4.1" "tree-sitter-markdown-inline/src")))  ;; pinned to ABI 14, matches this emacs
+  (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "v0.5.3" "tree-sitter-markdown/src"))
+  (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "v0.5.3" "tree-sitter-markdown-inline/src")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
