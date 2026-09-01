a7e03a3282ebd63d4d76089efc1b19775e771c7a
Thorsten Krüger <thorsten.krueger@uni-muenster.de>
2026-09-01 14:48:44 +0200

    extras/dev.el: remove duplicate markdown-ts-mode config
    Two use-package blocks were configuring markdown-ts-mode with
    conflicting tree-sitter-markdown grammar tags (split_parser vs
    v0.5.3), causing treesit-font-lock-rules-mismatch warnings. Keep
    only the "Markdown mode" section's block, which targets Emacs 31's
    built-in markdown-ts-mode.

--- extras/dev.el
+++ extras/dev.el
@@ -96,13 +96,6 @@
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package markdown-ts-mode
-  :mode ("\\.md\\'" . markdown-ts-mode)
-  :defer 't
-  :config
-  (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
-  (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src")))
-
 (use-package yaml-pro
   :ensure t
   :hook (yaml-ts-mode . yaml-pro-ts-mode))
